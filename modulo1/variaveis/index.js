16fcc7a5899b933e0ea060b7cd8f4069506f3df8
Christopher Feilstrecker <jokinha95@gmail.com>
2021-10-04 15:47:05 -0300

    Exercícios Variáveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,60 @@
+/*
+Exercícios de interpretação de código
+
+exercício 1) será impresso duas linhas, primeira linha com número 10
+segunda linha com os números 10 e 5
+
+exercício 2) será impresso 10 10 10
+
+exercício 3) 
+let horaPorDia = prompt("Quantas horas você trabalha por dia?")
+let valorPorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horaPorDia/valorPorDia} por hora`)
+
+*/
+
+//Exercícios de escrita de código
+
+//Exercício 1 -
+
+//let nome = 
+//let idade = 
+//console.log(typeof nome, typeof idade)
+
+//d) da erra na hora de imprimir o tipo, acredito que por não ter valor atribuido
+
+//let nome =  prompt("qual seu nome?")
+//let idade = prompt("qual sua idade?")
+//console.log(typeof nome, typeof idade)
+
+//f) com as variaveis definidas foi encontrado o tipo de variável
+
+//console.log("Olá", nome, "voce tem", idade, "anos")
+
+
+//Exercício 2 -
+
+//let tomouBanho = "sim"
+//let usouSabonete = "sim"
+//let lavouOCabelo = "sim"
+
+//console.log("Tomou banho hoje?",tomouBanho,"usou sabonete?",usouSabonete, ", lavou o cabelo", lavouOCabelo,
+// "Se  alguma das respostas for não, volte e tome banho.")
+
+//Exercicio 3
+//let a = 10
+//let b = 25
+
+ //c = a
+// a = b
+ //b = c
+//console.log("O novo valor de a é", a)
+//console.log("O novo valor de a é", b)
+
+//Desafio
+
+let a =  prompt("Digite um número inteiro?")
+let b =  prompt("Digite outro número inteiro?")
+
+console.log(a + b)
+console.log(a * b)
