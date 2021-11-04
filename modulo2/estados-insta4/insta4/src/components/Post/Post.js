b37cee14ace32340cba9905c125cd6ebce281bc9
Christopher <jokinha95@gmail.com>
2021-11-04 17:30:27 -0300

    Se lasquemo tudo...kkk

---
+++ modulo2/estados-insta4/insta4/src/components/Post/Post.js
@@ -0,0 +1,133 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import iconeEstrelaBranca from'https://media.istockphoto.com/vectors/star-icon-on-white-background-flat-style-white-star-icon-for-your-web-vector-id1158111903'
+import iconeEstrelaPreta from 'https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46106/star-emoji-clipart-md.png'
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
+
+class Post extends React.Component {
+  state = {
+    curtido: false,
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0,
+    salvar:false,
+  }
+
+  onClickCurtida = () => {
+const valorAtual = this.state.numeroCurtidas
+const curtindo = {numeroCurtidas: valorAtual + 1}
+const descurtindo = {numeroCurtidas: valorAtual - 1}
+if(this.state.numeroCurtidas===0){
+this.setState(curtindo)
+}else if(this.state.numeroCurtidas===1){
+  this.setState(descurtindo)
+}
+
+    console.log('Curtiu!')
+
+  }
+
+  onClickComentario = () => {
+    this.setState({
+      comentando: !this.state.comentando
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentando: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  render() {
+    let iconeCurtida
+
+    if(this.state.numeroCurtidas===1) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let iconeSalvar
+
+    if(this.state.numeroCusalvar) {
+      iconeSalvar = iconeEstrelaBranca
+    } else {
+      iconeSalvar = iconeEstrelaPreta
+    }
+
+
+    let componenteComentario
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    }
+
+    return <PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          salvar={iconeSalvar}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
+  }
+
+  
+}
+
+export default Post
