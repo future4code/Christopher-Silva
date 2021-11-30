d6fee0057c8d7fc108ffab2c839abb45b626a396
Christopher <jokinha95@gmail.com>
2021-11-30 14:29:50 -0300

    Exercícios de fixação useEffect

---
+++ modulo3/useEffect/aula32-use-effect/src/App.js
@@ -0,0 +1,49 @@
+import {useState,useEffect} from "react";
+import "./styles.css";
+import axios from "axios";
+import PokeCard from "./components/PokeCard/PokeCard";
+
+function App() {
+  const [pokeList, setPokelist] = useState([])
+  const [pokeName, setPokeName] = useState("")
+
+  
+ const pegaPokemons = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => {
+        setPokelist(response.data.results)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    pegaPokemons();
+  }, [pokeName]);
+
+ const changePokeName = event => {
+   setPokeName(event.target.value);
+  };
+
+    return (
+      <div className="App">
+        <select onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
+        </select>
+        {pokeName && <PokeCard pokemon={pokeName} />}
+      </div>
+    );
+  }
+
+
+export default App;
+
