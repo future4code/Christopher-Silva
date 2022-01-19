992abef7ea34144c9939ea3b35a51e28119bcc21
Christopher <jokinha95@gmail.com>
2022-01-19 15:43:51 -0300

    exercicios tipagem typescript

---
+++ modulo4/intro-typescript/build/ex2.js
@@ -0,0 +1,8 @@
+function imprimeTresCoresFavoritas() {
+    const cor1 = String(prompt("Insira sua primeira cor favorita"));
+    const cor2 = String(prompt("Insira sua segunda cor favorita"));
+    const cor3 = String(prompt("Insira sua terceira cor favorita"));
+    console.log([cor1, cor2, cor3]);
+}
+imprimeTresCoresFavoritas();
+//# sourceMappingURL=ex2.js.map
