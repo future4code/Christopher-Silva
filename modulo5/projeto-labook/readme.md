e1f6c78504ad298e8ebb40bc3a4b476ed15bc53c
Christopher <jokinha95@gmail.com>
2022-03-10 13:44:28 -0300

    Projeto labook

---
+++ modulo5/projeto-labook/readme.md
@@ -0,0 +1,42 @@
+# Labook API: <img src="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f44d.svg" width="25"> 
+
+### Projeto Labook API é uma cópia do banco de dados baseado no Facebook.
+
+
+## Documentação API:
+ Acesse o postman aqui 👉 [ <img src="https://cdn-icons-png.flaticon.com/512/3893/3893170.png" width="70" align="center"> ](https://documenter.getpostman.com/view/18385085/UVkpQGUM)
+
+## Deploy no Heroku:
+[] 
+
+## Desenvolvedor 🤖
+
+<table>
+  <tr>
+  <td align="center"><a href="https://github.com/ChristpherFeilstrecker">
+   <sub><b>Christopher Feilstrecker da Silva</b> </sub> 
+       
+</table>
+
+
+## Tecnologias utilizadas: 🖥️
+- Typescript
+- Node
+- SQL
+- MySQL
+- Express
+- Cors
+- Dotenv
+- Knex
+- React
+- Axios
+- UUID
+- Bcrypt
+- Hash
+- json
+
+
+
+## Os endpoints são:
+
+ → Pegar o próprio perfil
---
+++ modulo5/projeto-labook/src/business/PostBusiness.ts
@@ -0,0 +1,47 @@
+import PostData from "../data/PostData2";
+import Post, { PostInputDTO } from "../model/post";
+import { Authenticator } from "../services/Authenticator";
+import { HashManager } from "../services/HashManager";
+import { IdGenerator } from "../services/IdGenerator";
+
+export default class PostBusiness {
+    constructor(
+        private postData: PostData,
+        private idGenerator: IdGenerator,
+        private hashManager: HashManager,
+        private authenticator: Authenticator
+    ) { }
+
+    newPost = async (input: PostInputDTO,token:string) => {
+        const { photo_url, description, date_creat,type } = input
+        
+        if (!photo_url || !description || !date_creat || !type) {
+            throw new Error("Um ou mais campos não informados 'photo_url, description, date_creat ou type'")
+        }
+        if (!token) {
+            throw new Error("token não enviado")
+        }
+
+        const tokenData:any = await new Authenticator().getTokenData(token)
+
+        if (!tokenData) {
+            throw new Error("token invalido")
+        }
+        const user_id:string = tokenData.id
+        const id = this.idGenerator.generateId()
+
+        const post = new Post(
+            id,
+            user_id,
+            photo_url,
+            description,
+            date_creat,
+            type
+        )
+  
+        await this.postData.insert(post)
+
+        return 
+    }
+
+}
