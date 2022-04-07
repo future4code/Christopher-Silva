aa7b78c944e67a7fa13186655b80b5090737135a
Christopher <jokinha95@gmail.com>
2022-04-07 10:39:13 -0300

    add toModel

--- modulo6/rodada-cases-semana24/case-redfox/src/data/PokemonsDatabase.ts
+++ modulo6/rodada-cases-semana24/case-redfox/src/data/PokemonsDatabase.ts
@@ -1,15 +1,51 @@
+import { Pokemon, PokemonAllData } from "../model/Pokemon";
 import BaseDataBase from "./BaseDatabase";
 
 export class PokemonsDatabase extends BaseDataBase {
 
    protected tableName: string = "pokemon_go_api";
 
+   private toModel(dbModel?: any): Pokemon | undefined {
+      return (
+         dbModel &&
+         new Pokemon(
+            dbModel.id,
+            dbModel.name,
+            dbModel.img_name
+         )
+      );
+   }
+
+   private toModel2(modelAllData?: any): PokemonAllData | undefined {
+      return (
+         modelAllData &&
+         new PokemonAllData(
+            modelAllData.id,
+            modelAllData.name,
+            modelAllData.img_name,
+            modelAllData.generation,
+            modelAllData.evolution_stage,
+            modelAllData.type_1,
+            modelAllData.type_2,
+            modelAllData.weather_1,
+            modelAllData.weather_2,
+            modelAllData.stat_total,
+            modelAllData.atk,
+            modelAllData.def,
+            modelAllData.sta
+         )
+      );
+   }
+
    public async getAllPokemons(test:String): Promise<void|any> {
       try {
          const result = await BaseDataBase.connection.raw(`
          SELECT * from ${this.tableName};
          `);
-         return result[0];
+
+         return result[0].map((res: any) => {
+            return this.toModel(res);
+         });
       } catch (error) {
          if (error instanceof Error) {
             throw new Error(error.message) 
@@ -22,7 +58,10 @@ export class PokemonsDatabase extends BaseDataBase {
          const result = await BaseDataBase.connection.raw(`
          SELECT * from ${this.tableName} LIMIT ${page-1}, ${pokemonsForPage};
          `);
-         return result[0];
+
+         return result[0].map((res: any) => {
+            return this.toModel(res);
+         });
       } catch (error) {
          if (error instanceof Error) {
             throw new Error(error.message) 
@@ -35,7 +74,9 @@ export class PokemonsDatabase extends BaseDataBase {
          const result = await BaseDataBase.connection.raw(`
             SELECT * from ${this.tableName} WHERE id = '${id}'
          `);
-         return result[0];
+         return result[0].map((res: any) => {
+            return this.toModel2(res);
+         });
       } catch (error) {
          if (error instanceof Error) {
             throw new Error(error.message) 
@@ -51,7 +92,10 @@ export class PokemonsDatabase extends BaseDataBase {
             OR (type_1 LIKE '%${search}%')
             OR (type_2 LIKE '%${search}%')
          `);
-         return result[0];
+
+         return result[0].map((res: any) => {
+            return this.toModel(res);
+         });
       } catch (error) {
          if (error instanceof Error) {
             throw new Error(error.message) 
---
+++ modulo6/rodada-cases-semana24/case-redfox/src/model/Pokemon.ts
@@ -0,0 +1,90 @@
+
+export class Pokemon {
+    constructor(
+       private id: string,
+       private name: string,
+       private img_name: string
+    ) { }
+ 
+    public getId(): string {
+       return this.id;
+    }
+ 
+    public getName(): string {
+       return this.name;
+    }
+ 
+    public getImgName(): string {
+       return this.img_name;
+    }
+ }
+
+ export class PokemonAllData {
+    constructor(
+       private id: string,
+       private name: string,
+       private img_name: string,
+       private generation: string,
+       private evolution_stage: string,
+       private type_1: string,
+       private type_2: string,
+       private weather_1: string,
+       private weather_2: string,
+       private stat_total: number,
+       private atk: number,
+       private def: number,
+       private sta: number
+    ) { }
+ 
+    public getId(): string {
+       return this.id;
+    }
+ 
+    public getName(): string {
+       return this.name;
+    }
+ 
+    public getImgName(): string {
+       return this.img_name;
+    }
+
+    public getGeneration(): string {
+        return this.generation;
+     }
+
+    public getEvolutionStage(): string {
+        return this.evolution_stage;
+     }
+
+     public getType1(): string {
+        return this.type_1;
+     }
+
+     public getType2(): string {
+        return this.type_2;
+     }
+
+     public getWeather1(): string {
+        return this.weather_1;
+     }
+
+     public getWeather2(): string {
+        return this.weather_2;
+     }
+
+     public getStateTotal(): number {
+        return this.stat_total;
+     }
+
+     public getAtk(): number {
+        return this.atk;
+     }
+
+     public getDef(): number {
+        return this.def;
+     }
+
+     public getSta(): number {
+        return this.sta;
+     }
+ }
