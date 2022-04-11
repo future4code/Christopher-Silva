1a168e6b43fc01835b8f46adf09689172c200c33
Christopher <jokinha95@gmail.com>
2022-04-11 19:27:36 -0300

    exercício 5

--- modulo6/logic-exercises/src/ExercicioBusiness.ts
+++ modulo6/logic-exercises/src/ExercicioBusiness.ts
@@ -74,33 +74,50 @@ export class ExerciciosBusiness {
     }
   }
 
-  // let array:string[] = Array.from(str)
-  /*
-if (array.length <= 1) {
-      return ""
-    }
-    */
+
   public checkCommonPrefix(strs: string[]) { // recebe o array do console no index
 
-    if(strs.length === 0) return ""  //if que verifica se o que foi enviado estiver vaziu ja retorna um array vazio
-    
+    if (strs.length === 0) return ""  //if que verifica se o que foi enviado estiver vaziu ja retorna um array vazio
+
     let commonPrefix = ""  // variavel para salvar as letras iniciais iguais
 
-    for(let i = 0; i < strs[0].length; i++) { //for que pega o tamanho da primeira palavra
-        let currentChar = strs[0][i]  // var que pega letra por letra da primeira string
-        
-        for(let j = 0; j < strs.length; j++) {  //for que pega cada string do array enviado
-            if(strs[j][i] !== currentChar) {  // if que pega letra por letra da cada string, se for diferente retorna a var que vai salvar as letras iguais
-                return commonPrefix
-            }
+    for (let i = 0; i < strs[0].length; i++) { //for que pega o tamanho da primeira palavra
+      let currentChar = strs[0][i]  // var que pega letra por letra da primeira string
+
+      for (let j = 0; j < strs.length; j++) {  //for que pega cada string do array enviado
+        if (strs[j][i] !== currentChar) {  // if que pega letra por letra da cada string, se for diferente retorna a var que vai salvar as letras iguais
+          return commonPrefix
         }
-        
-        if(currentChar) {   // se passar no primeiro for é porque as letras são iguais, dai salva essa letra 
-            commonPrefix += currentChar
-        }
+      }
+
+      if (currentChar) {   // se passar no primeiro for é porque as letras são iguais, dai salva essa letra 
+        commonPrefix += currentChar
+      }
     }
-    
+
     return commonPrefix
+  }
+
+  public lonelyNumber(strs: number[]) {
+
+    let newArray = strs
+    for (let i = 0; i < strs.length; i++) {
+      for (let j = 1; i < strs.length; j++) {
+        if (strs[i] === strs[j]) {
+          
+            let buscarPor = strs[i]
+            let indice = newArray.indexOf(buscarPor);
+            while (indice >= 0) {
+              newArray.splice(indice, 1);
+              indice = newArray.indexOf(buscarPor);
+            }
+          
+
+        }
+      }
+    }
+
+    return newArray
   }
 }
 
--- modulo6/logic-exercises/src/index.ts
+++ modulo6/logic-exercises/src/index.ts
@@ -1,18 +1,20 @@
 import ExercicioBusiness from "./ExercicioBusiness";
 
 // Exercício do dia 1 - Número Faltante
- //console.log("Exercicio 1 - Array com número faltante na sequencia, resposta: ", ExercicioBusiness.ex1([3,4,5,6,8])) 
- //console.log("Exercicio 1 - Array sem número faltante na sequencia, resposta: ", ExercicioBusiness.ex1([1,2,3,4,5,6,7,8]))
+ //console.log("Exercicio 1 - resposta: ", ExercicioBusiness.ex1([3,4,5,6,8])) 
  //console.log("Exercicio 1 - simulando banco de dados, faltando número na sequencia, resposta: ", ExercicioBusiness.ex1BD("falta numero")) 
  //console.log("Exercicio 1 - simulando banco de dados, sem faltar número na sequencia, resposta: ", ExercicioBusiness.ex1BD("Não falta numero")) 
 
  // Exercício do dia 2 - indexOf
- //console.log("Exercicio 2 - Index onde aparece o caracter solicitado pela primera vez, resposta: ", ExercicioBusiness.indexOf("cavaleiro", "o"))
- //console.log("Exercicio 2 - retorno quando não encontra o caracter na palavra, resposta: ", ExercicioBusiness.indexOf("cavaleiro", "p"))
+ //console.log("Exercicio 2 - resposta: ", ExercicioBusiness.indexOf("cavaleiro", "o"))
+ 
 
  // Exercício do dia 3 - Checa parênteses válidos
- //console.log("Exercicio 3 - retorna false para caracter inválido, resposta: ", ExercicioBusiness.checkCharacter("({[])"))
- //console.log("Exercicio 3 - retorna true para caracter válido, resposta: ", ExercicioBusiness.checkCharacter("([{}])"))
-
+ //console.log("Exercicio 3 -  resposta: ", ExercicioBusiness.checkCharacter("({[])"))
+ 
   // Exercício do dia 4 - Maior prefixo comum
-  console.log("Exercicio 4 - retorna vazio quando não encontrado prefixo comum, resposta: ", ExercicioBusiness.checkCommonPrefix(["vida", "vivendo", "vivido"]))
+  //console.log("Exercicio 4 - resposta: ", ExercicioBusiness.checkCommonPrefix(["vida", "vivendo", "vivido"]))
+
+  // Exercício do dia 5 - O número solitário
+  console.log("Exercicio 5 - resposta: ", ExercicioBusiness.lonelyNumber([1,4,2,3,3,4,1]))
+ 
